e935056a407dca4d35a84e32446535c918fc6011
Sergio Cano <canosergio90@gmail.com>
2022-01-07 20:42:10 -0300

    list problems: get element at index and length method implementation

--- src/main/scala/io/github/rockthejvm/lists/ListProblems.scala
+++ src/main/scala/io/github/rockthejvm/lists/ListProblems.scala
@@ -8,6 +8,10 @@ sealed abstract class RList[+T] {
   def isEmpty: Boolean
 
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
+
+  def apply(index: Int): T
+
+  def length: Int
 }
 
 case object RNil extends RList[Nothing] {
@@ -16,10 +20,35 @@ case object RNil extends RList[Nothing] {
   override def isEmpty: Boolean     = true
 
   override def toString: String = "[]"
+
+  override def apply(index: Int): Nothing = throw new NoSuchElementException
+
+  override def length: Int = 0
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
   override def isEmpty: Boolean = false
+
+  override def apply(index: Int): T = {
+    @tailrec
+    def applyTailrec(list: RList[T], currentIndex: Int, index: Int): T =
+      list match {
+        case RNil           => throw new NoSuchElementException
+        case ::(head, tail) => if (currentIndex == index) head else applyTailrec(tail, currentIndex + 1, index)
+      }
+
+    if (index < 0) throw new NoSuchElementException
+    else applyTailrec(this, 0, index)
+  }
+
+  override def length: Int = {
+    @tailrec
+    def lengthTailRec(remaining: RList[T], result: Int): Int =
+      if (remaining.isEmpty) result
+      else lengthTailRec(remaining.tail, result + 1)
+
+    lengthTailRec(this, 0)
+  }
 
   override def toString: String = {
     @tailrec
